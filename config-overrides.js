0a96ae927c4ad46e49d1ef36253e401f45adb0c5
guoshiwei <guoshiwei@bjca.org.cn>
2021-07-09 09:40:15 +0800

    less的配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,15 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra')
 module.exports = override(
+  // 配置 antd-mobile 样式
        fixBabelImports('import', {
          libraryName: 'antd-mobile',
          style: 'css',
        }),
+  // 项目中使用less 开发  
+       addLessLoader({
+        lessOptions:{
+          javascriptEnabled:true
+        },
+        
+      })
      );
