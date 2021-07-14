da31e60b92b2a4fc3bdcb9f89903cb59bac5e06b
guoshiwei <guoshiwei@bjca.org.cn>
2021-07-14 10:36:39 +0800

    antd-mobile定制主题

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,10 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra')
 module.exports = override(
   // 配置 antd-mobile 样式
-       fixBabelImports('import', {
-         libraryName: 'antd-mobile',
-         style: 'css',
-       }),
+      //  fixBabelImports('import', {
+      //    libraryName: 'antd-mobile',
+      //    style: 'css',
+      //  }),
   // 项目中使用less 开发  
        addLessLoader({
         lessOptions:{
