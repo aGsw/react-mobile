f770d9f2f73accca64feaccbaf66a2eacd065737
guoshiwei <guoshiwei@bjca.org.cn>
2021-07-14 09:33:53 +0800

    绑定银行卡

--- src/pages/Project/hyxj/BankCard/index.jsx
+++ src/pages/Project/hyxj/BankCard/index.jsx
@@ -1,7 +1,11 @@
+import styles from './index.module.scss'
+import { Button } from 'antd-mobile'
 const BankCard = ()=>{
     return (
-        <div>
-            1231
+        <div className={styles.container}>
+            <img src={require('./images/无银行卡@2x.png').default} alt="" />
+            <p>暂无绑定的银行卡</p>
+            <Button type={'primary'}>去绑定</Button>
         </div>
     )
 }
